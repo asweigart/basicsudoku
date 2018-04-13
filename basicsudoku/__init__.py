eca38797950e458363773ee2239571ea00ce1002
Al Sweigart <asweigart@gmail.com>
2018-04-12 18:55:21 -0700

    Created BasicSolver class for solving puzzles.

--- basicsudoku/__init__.py
+++ basicsudoku/__init__.py
@@ -12,6 +12,7 @@ Some definitions used in this module:
     * row - The symbols going from left to right on the board.
     * symbols - A single digit from 1 to 9.
     * given - A symbol that is on the board at the start of a puzzle.
+    * peer - The spaces in the same row, column, or box as a specified space.
 
 >>> board = SudokuBoard()
 >>> print(board)
@@ -56,6 +57,10 @@ Some definitions used in this module:
 1 . 4 | . . . | . . .
 """
 
+import copy
+import time
+
+
 EMPTY_SPACE = '.'
 BOARD_LENGTH = 9
 BOARD_LENGTH_SQRT = 3 # square root of BOARD_LENGTH
@@ -87,10 +92,10 @@ class SudokuBoard(object):
         # When strict-mode is True, an exception will be raised if an illegal
         # symbol is placed on the board.
         self._strict = strict
-        self.clear_board()
+        self.clear_board() # make the board completely empty
 
         if symbols is not None:
-            self.symbols = symbols # use symbols property to populate _board
+            self.symbols = symbols # use the symbols property to populate _board
 
 
     @property
@@ -115,8 +120,16 @@ class SudokuBoard(object):
     def symbols(self):
         """Returns a string or tuple of all symbols on the board.
 
-        TODO"""
+        >>> board = SudokuBoard()
+        >>> board.symbols
+        '.................................................................................'
+        >>> board[0, 0] = '1'
+        >>> board.symbols
+        '1................................................................................'
+        """
         all_symbols = []
+
+        # Loop over all 81 spaces on the board.
         for y in range(BOARD_LENGTH):
             for x in range(BOARD_LENGTH):
                 all_symbols.append(self._board[x][y])
@@ -126,9 +139,12 @@ class SudokuBoard(object):
 
     @symbols.setter
     def symbols(self, value):
-        # Fill in the spaces with the provided symbols.
-        if not isinstance(value, str) or len(value) != FULL_BOARD_SIZE:
-            raise SudokuBoardException('symbols must be a string of 81 symbols')
+        # Make sure value has 81 symbols for all the spaces on the board.
+        try:
+            if len(value) != FULL_BOARD_SIZE:
+                raise SudokuBoardException('symbols must be a sequence of 81 symbols')
+        except TypeError:
+            raise SudokuBoardException('symbols must be a sequence of 81 symbols')
 
         # Check that all the symbols are valid.
         for symbol in value:
@@ -251,6 +267,7 @@ class SudokuBoard(object):
         except TypeError:
             raise SudokuBoardException('unit must be a sequence with exactly 9 symbols, not %r' % (unit,))
 
+        # Check to make sure all the symbols in the unit are valid.
         for symbol in unit:
             if not self.is_valid_symbol(symbol):
                 raise SudokuBoardException('unit contains an invalid symbol: %r' % (symbol,))
@@ -388,6 +405,7 @@ class SudokuBoard(object):
         if not isinstance(key, (tuple, list)) or len(key) != 2 or not isinstance(key[0], int) or not isinstance(key[1], int):
             raise SudokuBoardException('key must be a tuple of two integers')
 
+        # Split the key into x and y coordinates.
         x, y = key
         if x < 0 or x >= BOARD_LENGTH:
             raise SudokuBoardException('x index (%s) is out of range' % (x))
@@ -454,7 +472,7 @@ class SudokuBoard(object):
         elif not isinstance(key, (tuple, list)) or len(key) != 2 or not isinstance(key[0], int) or not isinstance(key[1], int):
             raise SudokuBoardException('key must be a tuple of two integers')
 
-        # Separate the x and y coordinates from key.
+        # Split the key into x and y coordinates.
         x, y = key
         if x < 0 or x >= BOARD_LENGTH:
             raise SudokuBoardException('x index (%s) is out of range' % (x))
@@ -465,7 +483,7 @@ class SudokuBoard(object):
         old_value = self._board[x][y]
         self._board[x][y] = value
 
-        # Do a board strictness check.
+        # Do a board validity check if in strict mode.
         if self._strict:
             if not self.is_valid_board():
                 self._board[x][y] = old_value # restore old value
@@ -559,12 +577,16 @@ class SudokuBoard(object):
         >>> _b1.get_box(0, 1)
         ['8', '.', '.', '4', '.', '.', '7', '.', '.']
         """
+
+        # Check that the x and y box coordinates are within range.
         if not isinstance(box_x, int) or box_x < 0 or box_x >= BOARD_LENGTH_SQRT:
             raise SudokuBoardException('box_x must be an int between 0 and 2')
 
         if not isinstance(box_y, int) or box_y < 0 or box_y >= BOARD_LENGTH_SQRT:
             raise SudokuBoardException('box_y must be an int between 0 and 2')
 
+        # Get the 9 symbols from the box, starting at the top left and going
+        # right and then down.
         box = []
         start_x = box_x * BOARD_LENGTH_SQRT
         start_y = box_y * BOARD_LENGTH_SQRT
@@ -615,6 +637,7 @@ class SudokuBoard(object):
         """
         all_rows = []
 
+        # Go through each row, gathering symbols for the string.
         for y in range(BOARD_LENGTH):
             row = self.get_row(y)
 
@@ -902,10 +925,218 @@ hardest = '''85...24..72......9..4.........1.7..23.5...9...4...........8..7..17.
 7.....4...2..7..8...3..8.799..5..3...6..2..9...1.97..6...3..9...3..4..6...9..1.35
 ....7..2.8.......6.1.2.5...9.54....8.........3....85.1...3.2.8.4.......9.7..6....'''.split('\n')
 
+
+class BasicSolver(object):
+    def __init__(self, board, timed=True):
+        self.board = board
+
+        if timed:
+            start_time = time.time()
+
+        self.solve()
+
+        if timed:
+            print('Solved in', round(time.time() - start_time, 2), 'seconds.')
+
+
+    def solve(self):
+        """Solve the sudoku puzzle as given in self.board, which is set to a
+        SudokuBoard object passed to __init__(). Returns True if a solution is
+        found, otherwise returns False.
+
+        There are two steps to solving. First, the possible candidates are
+        narrowed down based on the given symbols that the board starts with.
+
+        Second, the remaining possible candidates are searched through until
+        a solution is found. Multiple solutions are not detected; the first
+        solution found is the one returned. It is important to check the spaces
+        with the fewest candidates first, to cut down on the total number of
+        combinations that need to be searched.
+        """
+
+        # Each of the 81 spaces on the board has a set of all 9 symbols as
+        # candidates for the real symbol that belongs at that space. When this
+        # list is reduced to one symbol, we know we've solved that space.
+        board_candidates = [[set('123456789') for j in range(BOARD_LENGTH)] for i in range(BOARD_LENGTH)]
+
+        # Remove the given symbols that the board started with.
+        self.remove_givens_from_board_candidates(board_candidates)
+
+        # Search through all the remaining possibilities.
+        solution_symbols = self.solve_through_search(board_candidates)
+
+        if solution_symbols is not None:
+            # A solution was found, mark all the symbols on the board object.
+            self.board.symbols = solution_symbols
+            return True
+        else:
+            return False
+
+
+    def remove_givens_from_board_candidates(self, board_candidates):
+        """Remove the givens from all peer spaces. This function modifies
+        board_candidates in place."""
+        for i, symbol in enumerate(self.board):
+            if symbol != EMPTY_SPACE:
+                x, y = i % BOARD_LENGTH, i // BOARD_LENGTH
+                self.set_symbol(symbol, board_candidates, x, y)
+
+
+    def set_symbol(self, symbol, board_candidates, symbol_x, symbol_y):
+        """Set the symbol on the solver's SudokuBoard object, then remove that
+        symbol from all the peers of the space at symbol_x symbol_y. This may
+        cause other spaces to become solved, which will then call set_symbol()
+        again. This function modifies board_candidates in palce."""
+        symbol = str(symbol)
+        board_candidates[symbol_x][symbol_y] = set(symbol) # ensure that the board_candidates only have this symbol here
+        self.remove_from_peers(symbol, board_candidates, symbol_x, symbol_y)
+
+
+    def remove_from_peers(self, candidate, board_candidates, candidate_x, candidate_y):
+        """Remove the candidate from the peer spaces of candidate_x candidate_y
+        in board_candidates. This function modifies board_candidates in place."""
+        candidate = str(candidate)
+
+        # Remove candidate from the row of the xy space.
+        for x in range(BOARD_LENGTH):
+            if x != candidate_x:
+                self.remove_candidate(board_candidates, candidate, x, candidate_y)
+
+        # Remove candidate from the column of the xy space.
+        for y in range(BOARD_LENGTH):
+            if y != candidate_y:
+                self.remove_candidate(board_candidates, candidate, candidate_x, y)
+
+        # Remove candidate from the box of the xy space.
+        box_x, box_y = candidate_x // 3, candidate_y // 3 # Get the top left space of the box.
+        start_x = box_x * BOARD_LENGTH_SQRT
+        start_y = box_y * BOARD_LENGTH_SQRT
+        for y in range(start_y, start_y + BOARD_LENGTH_SQRT):
+            for x in range(start_x, start_x + BOARD_LENGTH_SQRT):
+                if x != candidate_x and y != candidate_y:
+                    self.remove_candidate(board_candidates, candidate, x, y)
+
+
+    def remove_candidate(self, board_candidates, candidate, x, y):
+        """Removes the candidate symbol from board_candidates at the x y space. This
+         function modifies board_candidates in place."""
+
+        if candidate in board_candidates[x][y]:
+            board_candidates[x][y].remove(candidate)
+            if len(board_candidates[x][y]) == 1:
+                # There is only one possible candidate for this space, menaing
+                # we've solved another space. Remove the symbol from the space's
+                # peers.
+                remaining_symbol = tuple(board_candidates[x][y])[0]
+                self.set_symbol(remaining_symbol, board_candidates, x, y)
+            elif len(board_candidates[x][y]) == 0:
+                raise SudokuBoardException('removing this candidate causes the board to be invalid')
+
+
+    def solve_through_search(self, board_candidates):
+        """Attempts a brute-force search of the possible solutions for the
+        board, and returns when found."""
+
+        # Check the space with the fewest candidates, to minimize the overall
+        # number of checks needed. (Don't include spaces that only have one
+        # candidate, i.e. spaces that are solved.)
+        order_of_spaces_to_check = [i for i in range(FULL_BOARD_SIZE) if len(board_candidates[i % BOARD_LENGTH][i // BOARD_LENGTH]) != 1]
+        order_of_spaces_to_check.sort(key=lambda i: len(board_candidates[i % BOARD_LENGTH][i // BOARD_LENGTH]), reverse=False)
+        if len(order_of_spaces_to_check) == 0:
+            # All the spaces have been solved, so lets just return the symbols that they form.
+            return self.make_board_from_candidates(board_candidates).symbols
+        space_to_check = order_of_spaces_to_check[0]
+
+        x, y = space_to_check % BOARD_LENGTH, space_to_check // BOARD_LENGTH
+        candidates = board_candidates[x][y]
+
+        assert len(candidates) > 0, 'board_candidates[%s][%s] has no candidates, which should never happen' % (x, y)
+
+        # Loop through all possible candidates for this space.
+        for candidate in candidates:
+            # This function is recursive, and testing each candidate in each
+            # call will need its own board_candidates.
+            board_candidates_copy = copy.deepcopy(board_candidates)
+
+            # Set this candidate as the only possible candidate in the copy
+            # of board_candidates, then test to see if the board it produces
+            # is valid.
+            board_candidates_copy[x][y] = set(candidate)
+            try:
+                self.remove_from_peers(candidate, board_candidates_copy, x, y)
+            except SudokuBoardException:
+                # Removing that candidate from the peers has caused a space to
+                # have zero candidates, meaning the board will be in an invalid
+                # state. So this candidate cannot be the solution and we should
+                # move on to the next candidate.
+                continue
+
+            board = self.make_board_from_candidates(board_candidates_copy)
+            if not board.is_valid_board():
+                # This candidate causes the board to become invalid, so this is
+                # not the correct solution for this space. Continue on to the
+                # next candidate.
+                continue
+
+            # If the board is both valid and full, the whole board is solved.
+            if board.is_full():
+                return board.symbols # BASE CASE
+
+            # Continue searching.
+            result = self.solve_through_search(board_candidates_copy) # RECUSIVE CASE
+            if result is not None:
+                return result # BASE CASE
+
+        # Exhausted all possible candidates and could not find a solution.
+        return None # BASE CASE
+
+
+    def make_board_from_candidates(self, board_candidates):
+        """Returns a SudokuBoard object, with the symbols set wherever the
+        board_candidates have only one possible candidate."""
+        symbols = []
+        for i in range(FULL_BOARD_SIZE):
+            x, y = i % BOARD_LENGTH, i // BOARD_LENGTH
+
+            assert len(board_candidates[x][y]) > 0, 'Somehow board_candidates at x %s, y %s has no candidates.' % (x, y)
+
+            if len(board_candidates[x][y]) == 1:
+                # If there is only one candidate, add it to the symbols list.
+                the_one_possible_candidate = tuple(board_candidates[x][y])[0]
+                symbols.append(the_one_possible_candidate)
+            else:
+                # If there are multiple candidates, mark it as an empty space.
+                symbols.append(EMPTY_SPACE)
+
+        return SudokuBoard(symbols=''.join(symbols), strict=False)
+
+
+
 if __name__ == '__main__':
     import doctest
     import pytest
     import os
 
+
     doctest.testmod()
     pytest.main([os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'tests'))])
+
+    # Easy puzzle (easy50), solvable through givens alone, no searching required.
+    board = SudokuBoard(symbols='..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..')
+    BasicSolver(board)
+    print(board, '\n')
+
+    # Easy puzzle (easy50), but requires searching.
+    board = SudokuBoard(symbols='2...8.3...6..7..84.3.5..2.9...1.54.8.........4.27.6...3.1..7.4.72..4..6...4.1...3')
+    BasicSolver(board)
+    print(board, '\n')
+
+    # Medium puzzle (top95), but requiressearching.
+    board = SudokuBoard(symbols='4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......')
+    BasicSolver(board)
+    print(board, '\n')
+
+    # Very hard puzzle (hardest).
+    board = SudokuBoard(symbols='85...24..72......9..4.........1.7..23.5...9...4...........8..7..17..........36.4.')
+    BasicSolver(board)
+    print(board, '\n')
